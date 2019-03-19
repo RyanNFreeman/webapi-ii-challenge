7270c53c65383b23742e9d7eb3a1e300428e081d
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-03-19 14:31:14 -0600

    very rough draft and initial commit.

---
+++ data/posts/posts-router.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const Posts = require('../db')
+
+const router = express.Router();
+
+router.post('/api/posts', (req, res) => {
+
+})
+
+router.get('/api/posts', (req, res) => {
+    
+})
+
+router.get('/api/posts/:id', (req, res) => {
+    
+})
+
+router.delete('/api/posts/:id', (req, res) => {
+    
+})
+
+router.put('/api/posts/:id', (req, res) => {
+    
+})
+
+module.exports = router
