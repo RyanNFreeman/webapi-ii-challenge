a5664fa0660caa2ae0d16f6b8e6978ac08166cb9
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-03-19 16:29:23 -0600

    CRUD operations are working with exception of delete needing a try catch.

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,22 +1,8 @@
 const express = require('express')
 
-const db = require('../data/db')
+const Posts = require('../data/db')
 
 const router = express.Router();
-
-//READ of CRUD operations
-
-router.get('/', (req, res) => {
-    //Returns an array of all the post objects contained in the database.
-    db
-    .find()
-    .then(posts => {
-        res.status(200).json(posts)
-    })
-    .catch(err => {
-        res.status(500).json({ error: "The post information could not be retrieved." })
-    })
-}) //WORKING NOW
 
 //CREATE of CRUD operations
 
@@ -24,7 +10,7 @@ router.post('/', (req, res) => {
     const newPost = req.body;
     const { title, contents } = newPost
     title && contents ? 
-    db
+    Posts
     .insert(newPost)
     .then(post => {
         res.status(201).json(post)
@@ -33,19 +19,70 @@ router.post('/', (req, res) => {
         res.status(500).json({ error: "There was an error while saving the post to the database" })
     }) :
     res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+}) // WORKING NOW
+
+//READ of CRUD operations
+
+router.get('/', (req, res) => {
+    //Returns an array of all the post objects contained in the database.
+    Posts
+    .find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+}) // WORKING NOW
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    Posts
+    .findById(id)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+}) // WORKING NOW
+
+//UPDATE of CRUD operations
+
+router.put('/:id', async (req, res) => {
+    const { title, contents } = req.body
+    if (title && contents) {
+        try {
+            const post = await Posts.update(req.params.id, { title, contents})
+            if (post > 0) {
+                res.status(200).json(post);
+            }
+            else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        } catch (err) {
+            res.status({ err: "The post information could not be modified."})
+        }
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    
 }) //WORKING NOW
 
+//DELETE of CRUD operations
 
-router.get('/api/posts/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     
-})
+    {const id = req.params.id
+    Posts
+    .remove(id)
+    .then(deleted => {
+        res.status(204).end()
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post could not be removed" })
+    })}
 
-router.delete('/api/posts/:id', (req, res) => {
-    
-})
-
-router.put('/api/posts/:id', (req, res) => {
-    
-})
+}) // mostly working. Need to do a try and catch for 404.
 
 module.exports = router
